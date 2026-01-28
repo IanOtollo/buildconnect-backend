4c086933567ae41711fdd95e2af5b3f7d0f225a9
IAN OTOLLO <ianotollo@gmail.com>
2026-01-28 17:30:06 +0300

    Fix CORS

--- config/database.php
+++ config/database.php
@@ -1,6 +1,15 @@
 <?php
 // Database configuration
-header('Access-Control-Allow-Origin: *');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: https://buildconnect-ke.vercel.app');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Max-Age: 3600');
+    http_response_code(200);
+    exit();
+}
+
+header('Access-Control-Allow-Origin: https://buildconnect-ke.vercel.app');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');

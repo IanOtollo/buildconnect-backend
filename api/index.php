5dd5f86afaef6db8b9b5fb8164c8843bbcc69071
IAN OTOLLO <ianotollo@gmail.com>
2026-02-16 14:53:54 +0300

    fix: Update CORS policy for Vercel compatibility

--- api/index.php
+++ api/index.php
@@ -1,6 +1,19 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+$allowed_origins = [
+    'https://buildconnect-ke.vercel.app',
+    'http://localhost:3000'
+];
+
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $origin);
+    header("Access-Control-Allow-Credentials: true");
+}
+else {
+    header("Access-Control-Allow-Origin: *");
+}
+
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
